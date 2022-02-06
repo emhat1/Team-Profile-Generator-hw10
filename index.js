f31d1bb555b8317af49fc2e854ae75203d2bfe88
Erin H <e.hatherell@gmail.com>
2022-02-06 22:55:15 +1100

    Addition of dependencies

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+
 // Setting up constructor functions
 const managerQuestions = require("./Library/ManagerQuestions.js");
 const engineerQuestions = require("./Library/EngineerQuestions.js");
 const internQuestions = require("./Library/InternQuestions.js");
+const Manager = require("./Library/Manager.js");
+const Engineer = require("./Library/Engineer.js");
+const Intern = require("./Library/Intern.js" );
 
 
 // Set up array for team profile data
