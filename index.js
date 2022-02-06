c863cef44a83f875ebde0e846b53b80bbd8f21b7
Erin Hatherell <93974950+emhat1@users.noreply.github.com>
2022-02-06 20:42:42 +1100

    Upload of rudimentary index.js file

---
+++ index.js
@@ -0,0 +1,15 @@
+// Setting up dependencies
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// Setting up constructor functions
+
+
+// Intake of information
+
+
+
+// Output to HTML file
+
+
+// Initialise function
