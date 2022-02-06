725d21f0cf54e19f78f0ef096437e72babf243ef
Erin H <e.hatherell@gmail.com>
2022-02-06 22:07:06 +1100

    Setting up for information intake

--- index.js
+++ index.js
@@ -7,9 +7,47 @@ const managerQuestions = require( "./Library/ManagerQuestions.js" );
 const engineerQuestions = require( "./Library/EngineerQuestions.js" );
 const internQuestions = require( "./Library/InternQuestions.js" );
 
+
+// Set up array for team profile data
+let teamProfileArray = [];
+
+
 // Intake of information
+function addManagerProfile() {
+    inquirer.prompt(managerQuestions)
+    .then(function(data) {
+       const managerName = data.managerName;
+       const managerId = data.managerId;
+       const managerEmail = data.managerEmail;
+       const managerOfficeNum = data.managerOffice;
+       const teamMember = new Manager(managerName, managerId, managerEmail, managerOffice);
+       teamProfileArr.push(teamMember);
+    });
+};
 
+function addEngineerProfile() {
+    inquirer.prompt(engineerQuestions)
+    .then (function(data) {
+       const engineerName = data.engineerName;
+       const engineerId = data.engineerId;
+       const engineerEmail = data.engineerEmail;
+       const engineerGithubUsername = data.engineerGithub;
+       const teamMember = new Engineer(engineerName, engineerId, engineerEmail, engineerGithub);
+       teamProfileArr.push(teamMember);
+    });
+};
 
+function addInternProfile() {
+    inquirer.prompt(internQuestions)
+    .then (function(data) {
+       const internName = data.internName;
+       const internId = data.internId;
+       const internEmail = data.internEmail;
+       const internSchool = data.internSchool;
+       const teamMember = new Intern(internName, internId, internEmail, internSchool);
+        teamProfileArr.push( teamMember );
+    });
+};
 
 // Output to HTML file
 
