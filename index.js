437392fd5122549a1bc5303d6e3b2ad2b8302238
Erin H <e.hatherell@gmail.com>
2022-02-06 21:50:42 +1100

    Addition of constructor functoions

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // Setting up constructor functions
-
+const managerQuestions = require( "./Library/ManagerQuestions.js" );
+const engineerQuestions = require( "./Library/EngineerQuestions.js" );
+const internQuestions = require( "./Library/InternQuestions.js" );
 
 // Intake of information
 
